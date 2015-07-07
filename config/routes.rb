26d0a66afa17e814ef8e255d70a4e5e0204bda67
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-07-07 14:46:50 +0200

    Add closed & opened routes of tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root 'tasks#index'
 
-  resources :tasks
+  resources :tasks do
+    member do
+      put 'closed'
+      put 'opened'
+    end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
